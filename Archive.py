200c4e86dd5e2e37faae89b9dc113321c375ba3c
Albert van Zyl <88828318+AlbertEE00@users.noreply.github.com>
2022-11-25 11:11:59 +0200

    Alot of stuff, but did the extract and view commands cleaned alot up

--- Archive.py
+++ Archive.py
@@ -18,16 +18,24 @@
 # USA
 
 import argparse
+import gzip
 import os
 import pathlib
+import re
+import shutil
+import zipfile
+from typing import List
+
+import py7zr
+import rarfile
 
 
-
-def get_file_name(filepath: str | os.path(), ext: bool) -> str:
-    """Returns the name of the file with or without extension.
+def get_file_name(filepath: str | pathlib.Path, ext: bool = False) -> str:
+    """
+    Returns the name of the file with or without extension.
 
     Args:
-        filepath (str | os.path): full path to file or relative to script.
+        filepath: Full path to file or relative to script.
         ext (bool): whether to return with extension.
 
     Returns:
@@ -35,62 +43,215 @@ def get_file_name(filepath: str | os.path(), ext: bool) -> str:
     """
     # if extension true return with extension
     if ext:
-        return os.path.basename((os.path.abspath(filepath)))
+        return str(os.path.basename((os.path.abspath(filepath))))
     # return without extension
-    return os.path.splitext(os.path.basename((os.path.abspath(filepath))))[0]
+    return str(os.path.splitext(os.path.basename((os.path.abspath(filepath))))[0])
 
 
-def get_file_extension(filepath: str | os.path()) -> str:
-    """_summary_
+def get_file_extension(filepath: str | pathlib.Path) -> str:
+    """
+    Returns the extension of the file.
 
     Args:
-        filepath (str | os.path): _description_
+        filepath: Full path to file or relative to script.
 
     Returns:
-        str: _description_
+        str: extension of file.
     """
     return pathlib.Path((os.path.abspath(filepath))).suffix
 
 
-def get_file_directory(filepath: str | os.path()) -> os.path():
-    return os.path.dirname((os.path.abspath(filepath)))
+def get_file_directory(filepath: str | pathlib.Path) -> pathlib.Path:
+    """
+    Return the directory of the file
+
+    Args:
+        filepath: Full path to file or relative to script.
+
+    Returns:
+        os.path(): Path to directory of file.
+
+    """
+    if os.path.isfile(os.path.abspath(filepath)):
+        return os.path.dirname((os.path.abspath(filepath)))
+    elif os.path.isdir(os.path.abspath(filepath)):
+        return os.path.abspath(filepath)
+
+
+def check_if_gz_file(filepath: str | pathlib.Path) -> bool:
+    GZIP_MAGIC_NUMBER = "31139"
+    f = open(filepath, mode='rb')
+    first_two = f.read(2)
+    if str(first_two[0]) + str(first_two[1]) == GZIP_MAGIC_NUMBER:
+        return True
+    else:
+        return False
 
 
 class ArchiveFile:
-    full_file_path = os.path()
+    full_file_path = pathlib.Path()
+    directory_path = pathlib.Path()
     filename = str()
     file_extension = str()
-    directory_path = os.path()
+    file_list = list()
+    __initState = False
 
     def __init__(self, file_path):
-        self.full_file_path = os.path.abspath(file_path)  # get full file path
+        file_path.replace("/", "\\")
+        if os.path.exists(os.path.abspath(file_path)):
+            self.full_file_path = pathlib.Path(os.path.abspath(file_path))  # get full file path
+            self.directory_path = self.full_file_path.parent.absolute()
+            self.filename = self.full_file_path.with_suffix("").name
+            self.file_extension = self.full_file_path.suffix
+            self.__initState = True
+        else:
+            print("Invalid File Path: exiting...")
+            exit()
+        self.read_contents()
+
+    def read_contents(self, print_files: bool = False, files: List[str] | None = None,
+                      file_types: List[str] | None = None,
+                      pattern: List[str] | None = None):
+        if not self.__initState:
+            return "Archive not initialized"
+        if rarfile.is_rarfile(self.full_file_path):
+            with rarfile.RarFile(self.full_file_path) as rar:
+                self.file_list = rar.namelist()
+        elif zipfile.is_zipfile(self.full_file_path):
+            with zipfile.ZipFile(self.full_file_path, "r") as zip_file:
+                self.file_list = zip_file.namelist()
+            # archive.extractall(path=None, members=None, pwd=None)
+        elif py7zr.is_7zfile(self.full_file_path):
+            with py7zr.SevenZipFile(self.full_file_path, "r") as SevenZip:
+                self.file_list = SevenZip.getnames()
+        elif check_if_gz_file(self.full_file_path):
+            self.file_list = [self.filename]
+        new_list = list()
+        if file_types or pattern or files:
+            for file in self.file_list:
+                append = False
+                if file_types:
+                    for f_type in file_types:
+                        if f_type in get_file_extension(file):
+                            append = True
+                if pattern:
+                    for search in pattern:
+                        res = re.findall(search, file)
+                        if res:
+                            append = True
+                if files:
+                    for file_name in files:
+                        res = re.findall(file_name, file)
+                        if res:
+                            append = True
+                if append:
+                    new_list.append(file)
+            self.file_list = new_list
+        if print_files:
+            for file in self.file_list:
+                print(file)
+        return self.file_list
+
+    def extract(self, extract_path: str = None, files: List[str] | None = None, file_types: List[str] | None = None,
+                pattern: List[str] | None = None):
+        if not self.__initState:
+            return "Archive not initialized"
+        # print(f'this is before: {extract_path}')
+        if extract_path is not None:
+            extract_path.replace("/", "\\")
+            if r'\\' in extract_path:
+                print("invalid path, exiting...")
+                exit()
+        if extract_path == "":  # extract to directory with file name
+            if check_if_gz_file(self.full_file_path):
+                extract_path = self.directory_path.joinpath(pathlib.Path(self.filename).with_suffix(""))
+            else:
+                extract_path = self.directory_path.joinpath(self.filename)
+            print(extract_path)
+            extract_path.mkdir(exist_ok=True)
+        else:
+            extract_path = pathlib.Path(
+                os.path.abspath(extract_path)) if extract_path else self.directory_path  # make a full path and
+        # path object
+        # print(f'this is after: {extract_path}')
+        # extract to directory with file name in archive directory
+
+        self.read_contents(False, files, file_types, pattern)
+
+        if rarfile.is_rarfile(self.full_file_path):
+            with rarfile.RarFile(self.full_file_path) as rar:
+                rar.extractall(path=extract_path, members=self.file_list, pwd=None)
+        elif zipfile.is_zipfile(self.full_file_path):
+            with zipfile.ZipFile(self.full_file_path, "r") as zipper:
+                zipper.extractall(path=extract_path, members=self.file_list, pwd=None)
+        elif py7zr.is_7zfile(self.full_file_path):
+            for i in range(self.file_list.__len__()):
+                py7zr.SevenZipFile(self.full_file_path, "r").extract(path=extract_path, targets=self.file_list[i])
+        elif check_if_gz_file(self.full_file_path):
+
+            extract_path = str(extract_path) + "\\" + self.filename
+            with gzip.open(self.full_file_path, 'rb') as f_in:
+                with open(extract_path, 'wb') as f_out:
+                    shutil.copyfileobj(f_in, f_out)
 
 
 def setup_parser():
     # help flag provides flag help
     # store_true actions stores argument as True
 
-    parser = argparse.ArgumentParser("Azoteq testing file sorter")
+    parser = argparse.ArgumentParser(prog="Archive.py", description="Archive management utility", epilog=" ")
     subparser = parser.add_subparsers(dest='command')
+    parser.add_argument('archive_path', help="Full path to file", type=str)
 
-    extract = subparser.add_parser('extract')
-    extract.add_argument('filepath', help="Full path to file", type=str)
-    extract.add_argument('-cd', '--currentdir', action='store_true',
-                         help="extract to current directory in a folder with file name")
-    extract.add_argument('-fn', '--foldername', type=str,
-                         help="extract to current directory in a folder with given folder name")
-    extract.add_argument('-p', '--path', type=str, help="extract to path entered")
-    extract.add_argument('-am', '--automove', action='store_true', help="automatically move to root dir")
-    extract.add_argument('-da', '--deletearchive', action='store_true',
-                         help="automatically delete archive after extract")
-    extract.add_argument('-t', '--type', action='append', help="extract only types specified")
-    extract.add_argument('-f', '--files', type=str, nargs="+", help="extract only files specified")
+    extract = subparser.add_parser('extract', help="extract file to specified location, if no arguments are given "
+                                                   "extract all files in archive to directory with name of file in "
+                                                   "the directory of the archive")
+    extract.add_argument('-d', '--destination', type=str, help="move file to specified location, if only a name is "
+                                                               "given then to a folder relative to archive location")
+    extract.add_argument('-cd', '--current_dir', action='store_true', help="extract to current directory")
+    extract.add_argument('-mat', '--move_all_to', type=str, help="move all files to a directory")
+    extract.add_argument('-matad', '--move_all_to_archive_dir', action='store_true', help="automatically move to "
+                                                                                          "archive directory")
+    extract.add_argument('-del', '--delete_archive', action='store_true',
+                         help="automatically delete archive after extraction")
 
-    # parser.add_argument('filepath', help="Full path to file",type=str)
-    parser.add_argument('-v', '--view', action='store_true', help="shows contents")
-    parser.add_argument('-m', '--move', type=str, help="move file to specified location")
+    extract.add_argument('-t', '--type', action='append', help="extract only types specified, appended with each -t")
+    extract.add_argument('-f', '--files', action='append', help="extract only files specified, appended with each -f")
+    extract.add_argument('-s', '--search', action='append', help="extract only matches to search specified, appended "
+                                                                 "with each "
+                                                                 "-s")
+
+    move = subparser.add_parser('move', help="move archive to specified location")
+    move.add_argument('destination', type=str, nargs='+', help="move file(s) to specified destination path, if only a "
+                                                               "name is "
+                                                               "given then to a folder relative to archive location")
+    view = subparser.add_parser('view', help="shows contents of archive")
+    view.add_argument('-t', '--type', action='append', help="view only types specified, appended with each -t")
+    view.add_argument('-f', '--files', action='append', help="view only files specified, appended with each -f")
+    view.add_argument('-s', '--search', action='append',
+                      help="view only matches to search specified, appended with each "
+                           "-s")
+
     return parser.parse_args()
 
 
 if __name__ == "__main__":
-    setup_parser()
+    args = setup_parser()
+    if args.command == "view":
+        ArchiveFile(args.file_path).read_contents(args.view, args.files, args.type, args.search)
+    elif args.command == "extract":
+        archive = ArchiveFile(args.archive_path)
+        destination_dir = ""
+        if args.current_dir:
+            destination_dir = None
+        elif args.destination:
+            destination_dir = args.destination
+        else:
+            destination_dir = ""
+        archive.extract(extract_path=destination_dir, files=args.files, file_types=args.type, pattern=args.search)
+        pass
+    elif args.command == "move":
+        pass
+    else:
+        print("Invalid command")
+        exit()
